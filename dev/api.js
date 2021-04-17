8e0fb4ce1270ee49ffe3be47fe020684db613eb0
LasseKau <44494614+LasseKau@users.noreply.github.com>
2021-04-17 09:37:37 +0300

    bodyparser
    -

--- dev/api.js
+++ dev/api.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3002
+//const bodyParser = require('body-parser');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 
 app.get('/blockchain', (req, res) => {
 
@@ -8,7 +13,7 @@ app.get('/blockchain', (req, res) => {
 
 app.post('/transaction', function (req, res) {
     console.log(req.body);
-    res.send('the amount of the transaction is {req.body.amount} bitcoin.');
+    res.send('the amount of the transaction is ${req.body.amount} bitcoin.');
 });
 
 app.get('/mine', function (req, res) {
