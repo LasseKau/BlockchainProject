af2608f3dd97a912220df5364cafb6205c022db3
LasseKau <44494614+LasseKau@users.noreply.github.com>
2021-04-19 09:16:01 +0300

    update
    -

--- dev/api.js
+++ dev/api.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const port = 3002
+const express = require('express');
+const app = express();
+const port = 3002;
 //const bodyParser = require('body-parser');
 
 app.use(express.json());
@@ -13,7 +13,7 @@ app.get('/blockchain', (req, res) => {
 
 app.post('/transaction', function (req, res) {
     console.log(req.body);
-    res.send('the amount of the transaction is ${req.body.amount} bitcoin.');
+    res.send(`the amount of the transaction is ${req.body.amount} bitcoin.`);
 });
 
 app.get('/mine', function (req, res) {
@@ -21,5 +21,5 @@ app.get('/mine', function (req, res) {
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`);
 })
