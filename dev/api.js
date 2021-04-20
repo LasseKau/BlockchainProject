b5ecbba845a6dc88abc5d1b7b29962029f8b7ef6
LasseKau <44494614+LasseKau@users.noreply.github.com>
2021-04-20 07:09:25 +0300

    Update api.js

--- dev/api.js
+++ dev/api.js
@@ -12,10 +12,10 @@ app.use(express.urlencoded({ extended: false }));
 app.get('/blockchain', (req, res) => {
     res.send(bitcoin);
 });
-
+//in out post transaction end ´point, we are creating a new transaction
 app.post('/transaction', function (req, res) {
-    console.log(req.body);
-    res.send(`the amount of the transaction is ${req.body.amount} bitcoin.`);
+    const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+    res.json({note: `Transaction will be added in block ${blockIndex}`});
 });
 
 app.get('/mine', function (req, res) {
