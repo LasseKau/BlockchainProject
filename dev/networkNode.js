ffdd8cf7a2085c2b103769a76c616262c0aef76e
LasseKau <44494614+LasseKau@users.noreply.github.com>
2021-04-23 13:11:27 +0300

    Update networkNode.js

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -59,8 +59,8 @@ app.get('/mine', function (req, res) {
     const lastBlock = bitcoin.getLastBlock();
     const previousBlockHash = lastBlock ['hash'];
     const currentBlockData = {
-        transactions: bitcoin.pendingTransactions,
-        index: lastBlock['index'] + 1
+		transactions: bitcoin.pendingTransactions,
+		index: lastBlock['index'] + 1
     };
     const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
     const blockHash = bitcoin.hashBlock(previousBlockHash,currentBlockData, nonce);
